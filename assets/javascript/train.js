341f5bacddda03e565e0545af27423c4d29664cc
Daniel Merkl <dan.merkl.90@gmail.com>
2019-08-15 22:41:28 -0500

    will figure out time tomorrow

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -32,6 +32,9 @@ document.getElementById("submitbtn").addEventListener("click", function (event){
     let arrival = 0;
     let minutes = 0;
     console.log(newName)
+
+
+
     document.getElementById("table-body").insertAdjacentHTML("beforeend",
     "<tr> <td>"+ newName
      + "</td> <td>" + newdestination
@@ -42,3 +45,45 @@ document.getElementById("submitbtn").addEventListener("click", function (event){
     document.getElementById("trainTime").value = ""
     document.getElementById("frequency").value = ""
 })
+
+function newTrainTimes(result){
+
+    let thomas = document.getElementById("table-body");
+    thomas.innerHTML = "";
+    for (let i = 0; i < result.length; i++){
+
+        let tFrequency = result[i].frequency;
+
+        let firstTime = result[i].trainTime;
+
+        let firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
+
+        // let currentTime = moment();
+
+        let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+
+        let tRemainder = diffTime % tFrequency;
+
+        let tMinutesTillTrain = tFrequency - tRemainder;
+
+        let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+
+        thomas.innerHTML += "<tr> <td>" + result[i].name 
+        + "</td> <td>" + result[i].frequency 
+        + "</td> <td>" + nextTrain
+         + "</td> <td>" + tMinutesTillTrain
+          +"</td> </tr>";
+    }};
+
+
+    getTrainData(function(result){ 
+    newTrainTimes(result)
+})
+
+    window.setInterval(function(){
+
+        getTrainData(function(result){
+            newTrainTimes(result);
+
+        });
+    },1000)
