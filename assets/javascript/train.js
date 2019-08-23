717f44515dddb445d7298368c77be327c18e4656
Daniel Merkl <dan.merkl.90@gmail.com>
2019-08-23 18:27:33 -0500

    timer is sort of there

--- assets/javascript/train.js
+++ assets/javascript/train.js
@@ -1,74 +1,80 @@
 
-// function getTrainData(cb){
-//     localforage.getItem("trainSchedule").then(function(result){
-//         cb(result || []);
-//     });
-// }
-// function setTrainData(NewTrainData, cb) {
-//     localforage.setItem("trainSchedule", NewTrainData).then(cb)
-// };
+function getTrainData(cb){
+    localforage.getItem("trainSchedule").then(function(result){
+        cb(result || []);
+    });
+}
+function setTrainData(NewTrainData, cb) {
+    localforage.setItem("trainSchedule", NewTrainData).then(cb)
+};
 
 document.getElementById("submitbtn").addEventListener("click", function (event){
     event.preventDefault();
-    let name = document.getElementById("trainName").value.trim()
-    let destination = document.getElementById("destination").value.trim()
-    let trainTime = document.getElementById("trainTime").value.trim()
-    let frequency = document.getElementById("frequency").value.trim()
+
     
-    localStorage.setItem("name",name)
-    localStorage.setItem("destination",destination)
-    localStorage.setItem("trainTime",trainTime)
-    localStorage.setItem("frequency",frequency)
+
+    let name = document.getElementById("trainName").value.trim();
+    let destination = document.getElementById("destination").value.trim();
+    let trainTime = document.getElementById("trainTime").value.trim();
+    let frequency = document.getElementById("frequency").value.trim();
+    
+
+    const newTrain = { 
+        name: name,
+        destination: destination,
+        trainTime: trainTime,
+        frequency: frequency
+       }
+
+       getTrainData(function(result){
+        let newArray = result;
+        newArray.push(newTrain)
+
+
     alert("Train has been added!")
-    console.log(name)
-    console.log(destination)
-    console.log(trainTime)
-    console.log(frequency)
+
     
-    let newName = localStorage.getItem("name")
-    let newdestination = localStorage.getItem("destination")
-    let newtrainTime = localStorage.getItem("trainTime")
-    let newfrequency = localStorage.getItem("frequency")
-    let arrival = 0;
-    let minutes = 0;
-    console.log(newName)
 
 
+setTrainData(newArray, function() {
+    console.log(newArray)
 
-    document.getElementById("table-body").insertAdjacentHTML("beforeend",
-    "<tr> <td>"+ newName
-     + "</td> <td>" + newdestination
-      + "</td> <td>" + newfrequency
-       + "</td> <td>" + arrival + "</td> <td>" + minutes + "</td> </tr>")
-    document.getElementById("trainName").value = ""
-    document.getElementById("destination").value = ""
-    document.getElementById("trainTime").value = ""
-    document.getElementById("frequency").value = ""
 })
+
+})
+});
+
+
 
 function newTrainTimes(result){
 
     let thomas = document.getElementById("table-body");
     thomas.innerHTML = "";
+
     for (let i = 0; i < result.length; i++){
 
         let tFrequency = result[i].frequency;
 
-        let firstTime = result[i].trainTime;
+        let firstTrainTime = result[i].trainTime;
 
         let firstTimeConverted = moment(firstTrainTime, "HH:mm").subtract(1, "years");
 
-        // let currentTime = moment();
+        let currentTime = moment();
+        console.log("CURRENT TIME: " + moment(currentTime).format("hh:mm"));
 
         let diffTime = moment().diff(moment(firstTimeConverted), "minutes");
+        console.log("DIFFERENCE IN TIME " + diffTime);
 
         let tRemainder = diffTime % tFrequency;
+
 
         let tMinutesTillTrain = tFrequency - tRemainder;
 
         let nextTrain = moment().add(tMinutesTillTrain, "minutes");
+        console.log("ARRIVAL TIME: " + moment(nextTrain).format("hh:mm"));
 
         thomas.innerHTML += "<tr> <td>" + result[i].name 
+        + "</td> <td>" + result[i].destination 
         + "</td> <td>" + result[i].frequency 
         + "</td> <td>" + nextTrain
          + "</td> <td>" + tMinutesTillTrain
